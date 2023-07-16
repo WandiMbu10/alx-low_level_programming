1c8b4fbb68c9f0288cdd837210d4868a8fa22ce1
WandiMbu10 <mbuyisaowethu46@gmail.com>
2023-07-16 14:09:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print alphabets lowercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++);
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n')
+	return (0);
+}
