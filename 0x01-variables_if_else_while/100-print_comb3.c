a879d8bba8eed81c449484c17671160199fff3d0
WandiMbu10 <mbuyisaowethu46@gmail.com>
2023-07-17 17:34:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int d, p;
 
-	for (d = '0';d < '9'; d++)
+	for (d = '0'; d < '9'; d++)
 	{
 		for (p = d + 1; p <= '9'; p++)
 		{
@@ -26,5 +26,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
