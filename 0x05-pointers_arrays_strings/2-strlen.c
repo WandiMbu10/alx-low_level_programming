26e6f2b89988adbd32875c781bc3e1dadbcd006a
WandiMbu10 <mbuyisaowethu46@gmail.com>
2023-07-25 14:37:03 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,9 +3,8 @@
 /**
  * _strlen - checks for  function that returns the length of a string
  * @s: char to be checked
- * return: always 0
+ * Return: always 0
  */
-
 int _strlen(char *s)
 {
 	int longi = 0;
